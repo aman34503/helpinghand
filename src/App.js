65679d1418c5721289eafb1b688a44eeecb58d11
isha614 <ishamehta614@gmail.com>
2021-09-11 14:26:16 +0530

    basic login page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+import React,{useState} from "react"
 import './App.css';
 
+import {useSpring,animated}from 'react-spring';
+
+
+import Start from "./loginPage/Start";
+
 function App() {
+  const [flip, set] = useState(false)
+  const props=useSpring({to:{opacity:1},from:{opacity:0.4}, 
+    reset: true,
+    reverse: flip,
+    delay: 700,
+
+    onRest: () => set(!flip),
+})
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <animated.div style={props}>
+      <center>
+            <h1 className="heading">Helping Hands, Welcomes You!</h1>
+            </center>
+            </animated.div>
+        <Start/>
+    </>
   );
 }
 
